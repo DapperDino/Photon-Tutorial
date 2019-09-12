ccf7c23d30e1e120231a9cb9c64c904c9037974d
DESKTOP-BRTMTMV\Nathan <natemi1010@gmail.com>
2019-09-12 18:54:45 +0100

    End Of Video 2 - Spawning & Moving

---
+++ Assets/Scripts/Movement/Movement.cs
@@ -0,0 +1,35 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+namespace PhotonTutorial.Movement
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class Movement : MonoBehaviourPun
+    {
+        [SerializeField] private float movementSpeed = 0f;
+
+        private CharacterController controller = null;
+
+        private void Start() => controller = GetComponent<CharacterController>();
+
+        void Update()
+        {
+            if (photonView.IsMine)
+            {
+                TakeInput();
+            }
+        }
+
+        private void TakeInput()
+        {
+            var movement = new Vector3
+            {
+                x = Input.GetAxisRaw("Horizontal"),
+                y = 0f,
+                z = Input.GetAxisRaw("Vertical"),
+            }.normalized;
+
+            controller.SimpleMove(movement * movementSpeed);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,12 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+namespace PhotonTutorial
+{
+    public class PlayerSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject playerPrefab = null;
+
+        private void Start() => PhotonNetwork.Instantiate(playerPrefab.name, Vector3.zero, Quaternion.identity);
+    }
+}
